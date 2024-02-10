6bf0fdee1331caea20a4ca767f9e60ec92088078
Ethiacl Universe <157616842+EthicalUniverse@users.noreply.github.com>
2024-02-11 00:06:01 +0600

    Create Run.py

---
+++ Run.py
@@ -0,0 +1,14 @@
+###-------[IMPORT MODULES]-----------####
+
+import os
+import sys
+import time
+import uuid
+import json
+import string
+import random
+import requests
+
+###=====Run=====###
+os.system("clear")
+os.system("python Fb-Bruteforce.py")
